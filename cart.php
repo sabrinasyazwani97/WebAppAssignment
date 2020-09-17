a89adb20bdf28382a0c32b8b1181c9a3504bd499
Sabrina Mazlan <54005211+sabrinasyazwani97@users.noreply.github.com>
2020-09-18 07:34:42 +0800

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Scarf4u | Ladies Trends No. 1</title>
+  <link href="cart.css" rel="stylesheet">
+</head>
+
+<body>
+  <header>
+      <?php include 'tudung-header.inc.php' ?>
+  </header>
+
+  <div>
+    <h2>YOUR CART IS CURRENTLY EMPTY</h2>
+    <p>You have not added any items in your shopping cart</p>
+    <button type="button" onclick="home.php">RETURN TO SHOP</button>
+  </div>
+
+  <footer>
+      <?php include 'tudung-footer.inc.php' ?>
+  </footer>
+</body>
+</html>
---
+++ sarung.php
@@ -0,0 +1,156 @@
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Scarf4u | Ladies Trends No. 1</title>
+  <link href="sarung.css" rel="stylesheet">
+</head>
+
+<body>
+
+<header>
+      <?php include 'scarf-header.inc.php' ?>
+  </header>
+
+  <h2>SARUNG</h2>
+  <div class="all_image">
+    <table>
+      <tr>
+        <td><div class="one_image"><img src="Image/Sarung/1.jpg" title="HAWWA SCARF – Instant Aliyah Kids Sarung Soft Blue" id="img"></div></td>
+        <td><div class="one_image"><img src="Image/Sarung/2.jpg" title="HAWWA SCARF – Instant Aliyah Kids Sarung Black" id="img"></div></td>
+        <td><div class="one_image"><img src="Image/Sarung/3.jpg" title="HAWWA SCARF – Instant Aliyah Kids Sarung Pink" id="img"></div></td>
+        <td><div class="one_image"><img src="Image/Sarung/4.jpg" title="HAWWA SCARF – Instant Aliyah Kids Sarung Brown" id="img"></div></td>
+      </tr>
+      <tr>
+        <td><p id="name">HAWWA SCARF – Instant Aliyah Kids Sarung Soft Blue</p></td>
+        <td><p id="name">HAWWA SCARF – Instant Aliyah Kids Sarung Black</p></td>
+        <td><p id="name">HAWWA SCARF – Instant Aliyah Kids Sarung Pink</p></td>
+        <td><p id="name">HAWWA SCARF – Instant Aliyah Kids Sarung Brown</p></td>
+      </tr>
+      <tr>
+        <td><text id="price">RM49.00</text></td>
+        <td><text id="price">RM49.00</text></td>
+        <td><text id="price">RM49.00</text></td>
+        <td><text id="price">RM49.00</text></td>
+      </tr>
+      <tr>
+        <td><button type="button" name="sarung1" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="button" name="sarung2" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="button" name="sarung3" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="button" name="sarung4" onclick="document.location='cart.php'">ADD TO CART</button></td>
+      </tr>
+    </table></br>
+
+    <table>
+      <tr>
+        <td><div class="one_image"><img src="Image/Sarung/5.jpg" title="HAWWA SCARF – Instant Aliyah Kids Sarung Navy Blue" id="img"></div></td>
+        <td><div class="one_image"><img src="Image/Sarung/6.jpg" title="HAWWA SCARF – Instant Aliyah Adult Bawal Brown" id="img"></div></td>
+        <td><div class="one_image"><img src="Image/Sarung/7.jpg" title="MAJEEDA – AUFA XXL GARNET RED" id="img"></div></td>
+        <td><div class="one_image"><img src="Image/Sarung/8.jpg" title="MAJEEDA – AUFA XXL DIJON YELLOW" id="img"></div></td>
+      </tr>
+      <tr>
+        <td><p id="name">HAWWA SCARF – Instant Aliyah Kids Sarung Navy Blue</p></td>
+        <td><p id="name">HAWWA SCARF – Instant Aliyah Adult Bawal Brown</p></td>
+        <td><p id="name">MAJEEDA – AUFA XXL GARNET RED</p></td>
+        <td><p id="name">MAJEEDA – AUFA XXL DIJON YELLOW</p></td>
+      </tr>
+      <tr>
+        <td><text id="price">RM49.00</text></td>
+        <td><text id="price">RM65.00</text></td>
+        <td><text id="price">RM49.00</text></td>
+        <td><text id="price"><text id="oldprice"/>RM49.00 </text><text id="promo"> RM44.10</text></text></td>
+      </tr>
+      <tr>
+        <td><button type="button" name="sarung5" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="button" name="sarung6" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="button" name="sarung7" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="button" name="sarung8" onclick="document.location='cart.php'">ADD TO CART</button></td>
+      </tr>
+    </table></br>
+
+    <table>
+      <tr>
+        <td><div class="one_image"><img src="Image/Sarung/9.jpg" title="MAJEEDA – AUFA XXL TURQUOISE" id="img"></div></td>
+        <td><div class="one_image"><img src="Image/Sarung/10.jpg" title="Voila Maxi Chiffon Rombo – C49" id="img"></div></td>
+        <td><div class="one_image"><img src="Image/Sarung/11.jpg" title="BOKITTA – Voila Printed Chiffon Calido" id="img"></div></td>
+        <td><div class="one_image"><img src="Image/Sarung/12.jpg" title="BOKITTA – Voila Maxi Chiffon Biwi" id="img"></div></td>
+      </tr>
+      <tr>
+        <td><p id="name">MAJEEDA – AUFA XXL TURQUOISE</p></td>
+        <td><p id="name">Voila Maxi Chiffon Rombo – C49</p></td>
+        <td><p id="name">BOKITTA – Voila Printed Chiffon Calido</p></td>
+        <td><p id="name">BOKITTA – Voila Maxi Chiffon Biwi</p></td>
+      </tr>
+      <tr>
+        <td><text id="price"><text id="oldprice"/>RM49.00 </text><text id="promo"> RM44.10</text></text></td>
+        <td><text id="price"><text id="oldprice"/>RM119.00 </text><text id="promo"> RM90.00</text></text></td>
+        <td><text id="price">RM90.00</text></td>
+        <td><text id="price">RM90.00</text></td>
+      </tr>
+      <tr>
+        <td><button type="button" name="sarung9" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="button" name="sarung10" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="button" name="sarung11" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="button" name="sarung12" onclick="document.location='cart.php'">ADD TO CART</button></td>
+      </tr>
+    </table></br>
+
+    <table>
+      <tr>
+        <td><div class="one_image"><img src="Image/Sarung/13.jpg" title="Voila Printed Chiffon Aztek" id="img"></br></div></br></td>
+        <td><div class="one_image"><img src="Image/Sarung/14.jpg" title="AIN WARDAH – IZARA EXPRESS HIJAB YELLOW" id="img"></div></td>
+        <td><div class="one_image"><img src="Image/Sarung/15.jpg" title="AIN WARDAH – IZARA EXPRESS HIJAB MUSTARD" id="img"></div></td>
+        <td><div class="one_image"><img src="Image/Sarung/16.jpg" title="AIN WARDAH – IZARA EXPRESS HIJAB PEACH" id="img"></div></td>
+      </tr>
+      <tr>
+        <td><p id="name">Voila Printed Chiffon Aztek</p></td>
+        <td><p id="name">AIN WARDAH – IZARA EXPRESS HIJAB YELLOW</p></td>
+        <td><p id="name">AIN WARDAH – IZARA EXPRESS HIJAB MUSTARD</p></td>
+        <td><p id="name">AIN WARDAH – IZARA EXPRESS HIJAB PEACH</p></td>
+      </tr>
+      <tr>
+        <td><text id="price"><text id="oldprice"/>RM109.00 </text><text id="promo"> RM90.00</text></text></td>
+        <td><text id="price">RM59.00</text></td>
+        <td><text id="price">RM59.00</text></td>
+        <td><text id="price">RM59.00</text></td>
+      </tr>
+      <tr>
+        <td><button type="button" name="sarung13" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="button" name="sarung14" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="button" name="sarung15" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="button" name="sarung16" onclick="document.location='cart.php'">ADD TO CART</button></td>
+      </tr>
+    </table></br>
+
+    <table>
+      <tr>
+        <td><div class="one_image"><img src="Image/Sarung/17.jpg" title="IZARA EXPRESS HIJAB MERAH BATA – D04" id="img"></div></td>
+        <td><div class="one_image"><img src="Image/Sarung/18.jpg" title="IZARA EXPRESS HIJAB FUSCHIA – D04" id="img"></div></td>
+        <td><div class="one_image"><img src="Image/Sarung/19.jpg" title="NYZANOUR – LILY 3.0 EXPRESS (SOFT AWNING) BLUE" id="img"></div></td>
+        <td><div class="one_image"><img src="Image/Sarung/20.jpg" title="GALERI TUDUNG MAHSUZ – Instant Twist Exclusive El Raissa White Black" id="img"></div></td>
+      </tr>
+      <tr>
+        <td><p id="name">IZARA EXPRESS HIJAB MERAH BATA – D04</p></td>
+        <td><p id="name">IZARA EXPRESS HIJAB FUSCHIA – D04</p></td>
+        <td><p id="name">NYZANOUR – LILY 3.0 EXPRESS (SOFT AWNING) BLUE</p></td>
+        <td><p id="name">GALERI TUDUNG MAHSUZ – Instant Twist Exclusive El Raissa White Black</p></td>
+      </tr>
+      <tr>
+        <td><text id="price">RM59.00</text></td>
+        <td><text id="price">RM59.00</text></td>
+        <td><text id="price"><text id="oldprice"/>RM53.00 </text><text id="promo"> RM39.75</text></text></td>
+        <td><text id="price">RM49.00</text></td>
+      </tr>
+      <tr>
+        <td><button type="button" name="sarung17" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="button" name="sarung18" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="button" name="sarung19" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="button" name="sarung20" onclick="document.location='cart.php'">ADD TO CART</button></td>
+      </tr>
+    </table></br>
+  </div>
+
+  <footer>
+      <?php include 'scarf-footer.inc.php' ?>
+  </footer>
+</body>
+</html>
