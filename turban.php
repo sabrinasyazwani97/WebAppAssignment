bb3f42383cb03d6fc5f678e066131f95e827f5d1
shakinassi <ohhshakina@gmail.com>
2020-09-19 14:32:54 +0800

    bawal and turban

--- turban.php
+++ turban.php
@@ -15,33 +15,33 @@
 	<div class="alignRow">
 		<div class="imageColumn">
 		<img src="images/BASIC-WHITE.jpg" alt="BASIC-WHITE" style="width:90%">
-		<p id="name">Anisalidrus Turban – Basic White</p></td>
-		<p id="price">RM 50.00</p></td>
+		<p id="name">Anisalidrus Turban – Basic White</p>
+		<p id="price">RM 50.00</p>
 		<button type="submit" name="bawal1" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
 	</div>
 	
 		<div class="imageColumn">
 		<img src="images/BASIC-BLUE.jpg" alt="BASIC-BLUE"  style="width:90%">
-		<p id="name">Anisalidrus Turban – Basic Blue</p></td>
-		<p id="price">RM 50.00</p></td>
+		<p id="name">Anisalidrus Turban – Basic Blue</p>
+		<p id="price">RM 50.00</p>
 		<button type="submit" name="bawal2" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
 	</div>
 		<div class="imageColumn">
 		<img src="images/DENIM-PINK.jpg" alt="DENIM-PINK" style="width:90%">
-		<p id="name">Anisalidrus Turban – Turban Denim Pink</p></td>
-		<p id="price">RM 55.00</p></td>
+		<p id="name">Anisalidrus Turban –  Denim Pink</p>
+		<p id="price">RM 55.00</p>
 		<button type="submit" name="bawal3" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
 	</div>
 		<div class="imageColumn">
 		<img src="images/DENIM-BLUE.jpg" alt="DENIM-BLUE" style="width:90%">
-		<p id="name">Anisalidrus Turban – Turban Denim Blue</p></td>
-		<p id="price">RM 55.00</p></td>
+		<p id="name">Anisalidrus Turban – Denim Blue</p>
+		<p id="price">RM 55.00</p>
 		<button type="submit" name="bawal4" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
 	</div>
 		<div class="imageColumn">
 		<img src="images/DENIM-GREEN.jpg" alt="DENIM-GREEN"  style="width:90%">
-		<p id="name">Anisalidrus Turban – Turban Denim Green</p></td>
-		<p id="price">RM 55.00</p></td>
+		<p id="name">Anisalidrus Turban – Denim Green</p>
+		<p id="price">RM 55.00</p>
 		<button type="submit" name="bawal5" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
 	</div>
 </div>
@@ -49,32 +49,32 @@
 	<div class="alignRow2">
 		<div class="imageColumn2">
 		<img src="images/DENIM-MUSTARD.jpg" alt="DENIM-MUSTARD"  style="width:90%">
-		<p id="name">Anisalidrus Turban – Turban Denim Mustard</p></td>
-		<p id="price">RM 55.00</p></td>
+		<p id="name">Anisalidrus Turban –  Denim Mustard</p>
+		<p id="price">RM 55.00</p>
 		<button type="submit" name="bawal6" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
 	</div>
 		<div class="imageColumn2">
 		<img src="images/DENIM-GREY.jpg" alt="DENIM-GREY"  style="width:90%">
-		<p id="name">Anisalidrus Turban – Turban Denim Grey</p></td>
-		<p id="price">RM 55.00</p></td>
+		<p id="name">Anisalidrus Turban –  Denim Grey</p>
+		<p id="price">RM 55.00</p>
 		<button type="submit" name="bawal7" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
 	</div>
 		<div class="imageColumn2">
 		<img src="images/FEMME-PRETTY-PINK.jpg" alt="FEMME-PRETTY-PINK"   style="width:90%">
-		<p id="name">Anisalidrus Turban – Turban Femme Pretty Pink</p></td>
-		<p id="price">RM 75.00</p></td>
+		<p id="name">Anisalidrus Turban – Turban Femme Pretty Pink</p>
+		<p id="price">RM 75.00</p>
 		<button type="submit" name="bawal8" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
 	</div>
 		<div class="imageColumn2">
 		<img src="images/FEMME-MINT-GREEN.jpg" alt="FEMME-MINT-GREEN"  style="width:90%">
-		<p id="name">Anisalidrus Turban – Turban Femme Mint Green</p></td>
-		<p id="price">RM 75.00</p></td>
+		<p id="name">Anisalidrus Turban – Turban Femme Mint Green</p>
+		<p id="price">RM 75.00</p>
 		<button type="submit" name="bawal9" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
 	</div>
 		<div class="imageColumn2">
 		<img src="images/FEMME-CHOCO-GREY.jpg" alt="FEMME-CHOCO-GREY"   style="width:90%">
-		<p id="name">Anisalidrus Turban – Turban Femme Choco Grey</p></td>
-		<p id="price">RM 75.00</p></td>
+		<p id="name">Anisalidrus Turban – Turban Femme Choco Grey</p>
+		<p id="price">RM 75.00</p>
 		<button type="submit" name="bawal10" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
 	</div>
 </div>
@@ -82,8 +82,8 @@
 	<div class="alignRow3">
 		<div class="imageColumn3">
 		<img src="images/FEMME-BABY-BLUE.jpg" alt="FEMME-BABY-BLUE"  style="width:90%">
-		<p id="name">Anisalidrus Turban – Turban Femme Baby Blue</p></td>
-		<p id="price">RM 75.00</p></td>
+		<p id="name">Anisalidrus Turban – Turban Femme Baby Blue</p>
+		<p id="price">RM 75.00</p>
 		<button type="submit" name="bawal11" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
 </div>
 
