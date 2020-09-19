af9725c87e0a0a2d6dbc52e36fc491954df29caf
alyaamn <71474184+alyaamn@users.noreply.github.com>
2020-09-19 18:46:10 +0800

    Add files via upload

---
+++ niqab.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+
+<html>
+	<head lang = "en">
+		<meta charset = "utf-8">
+		<title>Niqab | Scarf4u</title>
+		<link rel="stylesheet" href="niqab.css" />
+		<link rel="cart" href "cart.php"</a>
+	</head>
+	
+	<body>
+		<header>
+			<h1 class = "title">NIQAB</h1>
+		</header>
+		
+		<div>
+			<div class = "niqab">
+				<div class = "row">
+					<div class="col">
+						<img src= "photo/belani-black.jpg">
+						<h3>Niqab Belani - Black</h3>
+						<p>RM20.00</p>
+						<button type="submit" name="niqab1" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/belani-brown.jpg">
+						<h3>Niqab Belani - Brown</h3>
+						<p>RM20.00</p>
+						<button type="submit" name="niqab2" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/belani-green.jpg">
+						<h3>Niqab Belani - Emerald Green</h3>
+						<p>RM20.00</p>
+						<button type="submit" name="niqab3" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/belani-pink.jpg">
+						<h3>Niqab Belani - Charmine Pink</h3>
+						<p>RM20.00</p>
+						<button type="submit" name="niqab4" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+				</div>
+
+				<div class = "row">
+					<div class="col">
+						<img src= "photo/melani-darkblue.jpg">
+						<h3>Niqab Melani - Dark Blue</h3>
+						<p>RM35.00</p>
+						<button type="submit" name="niqab5" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/melani-maroon.jpg">
+						<h3>Niqab Melani - Maroon</h3>
+						<p>RM35.00</p>
+						<button type="submit" name="niqab6" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+				
+					<div class="col">
+						<img src= "photo/khimar-black.jpg">
+						<h3>Niqab Khimar - Black</h3>
+						<p>RM69.00</p>
+						<button type="submit" name="niqab7" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/khimar-white_black.jpg">
+						<h3>Niqab Khimar - White & Black</h3>
+						<p>RM69.00</p>
+						<button type="submit" name="niqab8" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+				</div>
+					
+				<div class = "row">
+					<div class="col">
+						<img src= "photo/khimar-sumayyah.jpg">
+						<h3>Niqab Khimar (Sumayyah) - Black</h3>
+						<p>RM69.00</p>
+						<button type="submit" name="niqab9" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/khimar-sara4xl.jpg">
+						<h3>Niqab Khimar (Sara 4XL) - Black</h3>
+						<p>RM99.00</p>
+						<button type="submit" name="niqab10" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+				
+					<div class="col">
+						<img src= "photo/khimar_sara4xl-latte.jpg">
+						<h3>Niqab Khimar (Sara 4XL) - Latte</h3>
+						<p>RM99.00</p>
+						<button type="submit" name="niqab11" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/sara-black.jpg">
+						<h3>Niqab Sara - Black</h3>
+						<p>RM39.00</p>
+						<button type="submit" name="niqab12" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+				</div>
+				
+				<div class = "row">
+					<div class="col">
+						<img src= "photo/fiona.jpg">
+						<h3>Niqab Fiona - Black</h3>
+						<p>RM55.00</p>
+						<button type="submit" name="niqab13" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/laora-black.jpg">
+						<h3>Niqab Laora - Black</h3>
+						<p>RM69.00</p>
+						<button type="submit" name="niqab14" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/layla-black.jpg">
+						<h3>Niqab Layla - Black</h3>
+						<p>RM39.00</p>
+						<button type="submit" name="niqab15" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+						
+					<div class="col">
+						<img src= "photo/sofya-black.jpg">
+						<h3>Niqab Sofya - Black</h3>
+						<p>RM65.00</p>
+						<button type="submit" name="niqab16" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>					
+				</div>
+			</div>
+		</div>
+		
+	</body>
+
+</html>
---
+++ shawl.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Shawl | Scarf4u</title>
+		<link rel="stylesheet" href="shawl.css" />
+	</head>
+	
+	<body>
+		<header>
+			<h1 class = "title">SHAWL</h1>
+		</header>
+		
+		<div>
+			<div class = "shawl">
+				<div class = "row">
+					<div class="col">
+						<img src= "photo/naelofar_eva-candypink.jpg">
+						<h4>Naelofar</h4>
+						<h3>Instant Shawl Eva - Candy Pink</h3>
+						<p>RM79.00</p>
+						<button type="submit" name="shawl1" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/naelofar_eva-ambrosia.jpg">
+						<h4>Naelofar</h4>
+						<h3>Instant Shawl Eva - Ambrosia</h3>
+						<p>RM79.00</p>
+						<button type="submit" name="shawl2" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/lady_semi-plainblue.jpg">
+						<h4>Lady Arrisa Couture</h4>
+						<h3>Semi Instant Hijab <br> Audrey Exclusive - Plain Blue</h3>
+						<p>RM129.90</p>
+						<button type="submit" name="shawl3" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/lady_semi-plaingreen.jpg">
+						<h4>Lady Arrisa Couture</h4>
+						<h3>Semi Instant Hijab <br> Audrey Exclusive - Plain Green</h3>
+						<p>RM129.90</p>
+						<button type="submit" name="shawl4" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+				</div>
+
+				<div class = "row">
+					<div class="col">
+						<img src= "photo/rayzara_alyah-purple.jpg">
+						<h4>Rayzara</h4>
+						<h3>Instant Shawl Alyah - Purple</h3>
+						<p>RM55.00</p>
+						<button type="submit" name="shawl5" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/rayzara_alyah-red.jpg">
+						<h4>Rayzara</h4>
+						<h3>Instant Shawl Alyah - Red</h3>
+						<p>RM55.00</p>
+						<button type="submit" name="shawl6" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+				
+					<div class="col">
+						<img src= "photo/rayzara_shayla-purple.jpg">
+						<h4>Rayzara</h4>
+						<h3>Satin Shawl Shayla - Purple</h3>
+						<p>RM39.00</p>
+						<button type="submit" name="shawl7" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/rayzara_shayla-turquoise.jpg">
+						<h4>Rayzara</h4>
+						<h3>Satin Shawl Shayla - Turquoise</h3>
+						<p>RM39.00</p>
+						<button type="submit" name="shawl8" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+				</div>
+					
+				<div class = "row">
+					<div class="col">
+						<img src= "photo/rayzara-instant_shawl-black.jpg">
+						<h4>Rayzara</h4>
+						<h3>Instant Shawl - Black</h3>
+						<p>RM35.00</p>
+						<button type="submit" name="shawl9" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/rayzara-instant_shawl-red.jpg">
+						<h4>Rayzara</h4>
+						<h3>Instant Shawl - Red Chilli</h3>
+						<p>RM35.00</p>
+						<button type="submit" name="shawl10" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+				
+					<div class="col">
+						<img src= "photo/meylana-scarf-flowers.jpg">
+						<h4>Meylana</h4>
+						<h3>Square Scarf Flowers - Purple</h3>
+						<p>RM89.00</p>
+						<button type="submit" name="shawl11" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/meylana-scarf-flowers1.jpg">
+						<h4>Meylana</h4>
+						<h3>Square Scarf Flowers - Peach</h3>
+						<p>RM89.00</p>
+						<button type="submit" name="shawl12" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+				</div>
+				
+				<div class = "row">
+					<div class="col">
+						<img src= "photo/adara-ozel_collection_egypt.jpg">
+						<h4>Adara</h4>
+						<h3>Ozel Collection Eqypt</h3>
+						<p>RM159.00</p>
+						<button type="submit" name="shawl13" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/adara-ozel_collection_qatar.jpg">
+						<h4>Adara</h4>
+						<h3>Ozel Collection Qatar</h3>
+						<p>RM159.00</p>
+						<button type="submit" name="shawl14" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+					
+					<div class="col">
+						<img src= "photo/fateema_dream-masonblue.jpg">
+						<h4>Fateema Couture</h4>
+						<h3>Shawl Dream Catcher - Mason Blue</h3>
+						<p>RM49.00</p>
+						<button type="submit" name="shawl15" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>
+						
+					<div class="col">
+						<img src= "photo/fateema_dream-mustard.jpg">
+						<h4>Fateema Couture</h4>
+						<h3>Shawl Dream Catcher - Mustard</h3>
+						<p>RM49.00</p>
+						<button type="submit" name="shawl16" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+					</div>					
+				</div>
+			</div>
+		</div>
+	
+	</body>
+
+</html>
