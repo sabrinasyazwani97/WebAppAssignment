e933d59ac73d1df1a66ea48e1543a6d0cc4d00bd
Sabrina Mazlan <54005211+sabrinasyazwani97@users.noreply.github.com>
2020-09-19 18:28:40 +0800

    Update cart.php

--- cart.php
+++ cart.php
@@ -11,48 +11,47 @@
   </header>
 
   <div>
-    <div>
-      <h2>YOUR CART IS CURRENTLY EMPTY</h2>
-      <p>You have not added any items in your shopping cart</p>
-      <button type="button" onclick="document.location='home.php'">RETURN TO SHOP</button>
-    </div></br></br>
+    <h2>YOUR CART IS CURRENTLY EMPTY</h2>
+    <p>You have not added any items in your shopping cart</p>
+    <button type="button" onclick="document.location='home.php'">RETURN TO SHOP</button>
+  </div></br></br>
 
-    <table>
-      <div>
-        <tr>
-          <th class="coupon_code" colspan="2">HAVE A COUPON?</th>
-          <th id="blank"></th>
-          <th class="total_cart" colspan="2">CART TOTALS</th>
-        </tr>
-        <tr>
-          <td id="col1"><input type="text" name="coupon" class="couponText"></input></td>
-          <td id="col2"><button type="submit" name="code" class="code_submit" onclick="document.location='checkout.php'">APPLY COUPON</button></td>
-          <td id="blank"></td>
-          <td id="col1">SUBTOTAL</td>
-          <td id="col2">RMXX<?php $subtotal ?></td>
-        </tr>
-        <tr>
-          <td id="blank"></td>
-          <td id="blank"></td>
-          <td id="blank"></td>
-          <td id="col1">SHIPPING</td>
-          <td id="col2">RMXX<?php $shipping_fee?></td>
-        </tr>
-        <tr>
-          <td id="blank"></td>
-          <td id="blank"></td>
-          <td id="blank"></td>
-          <td id="col1">TOTAL</td>
-          <td id="col_total">RMXX<?php $total?></td>
-        </tr>
-        <tr>
-          <td id="blank"></td>
-          <td id="blank"></td>
-          <td id="blank"></td>
-          <td id="blank"></td>
-          <td><button type="submit" name="checkout" class="checkoutBtn" onclick="document.location='checkout.php'">PROCEED TO CHECKOUT</button></td>
-        </tr>
-      </table>
+  <table>
+    <div>
+      <tr>
+        <th class="coupon_code" colspan="2">HAVE A COUPON?</th>
+        <th id="blank"></th>
+        <th class="total_cart" colspan="2">CART TOTALS</th>
+      </tr>
+      <tr>
+        <td id="col1"><input type="text" name="coupon" class="couponText"></input></td>
+        <td id="col2"><button type="submit" name="code" class="code_submit" onclick="document.location='checkout.php'">APPLY COUPON</button></td>
+        <td id="blank"></td>
+        <td id="col1">SUBTOTAL</td>
+        <td id="col2">RMXX<?php $subtotal ?></td>
+      </tr>
+      <tr>
+        <td id="blank"></td>
+        <td id="blank"></td>
+        <td id="blank"></td>
+        <td id="col1">SHIPPING</td>
+        <td id="col2">RMXX<?php $shipping_fee?></td>
+      </tr>
+      <tr>
+        <td id="blank"></td>
+        <td id="blank"></td>
+        <td id="blank"></td>
+        <td id="col1">TOTAL</td>
+        <td id="col_total">RMXX<?php $total?></td>
+      </tr>
+      <tr>
+        <td id="blank"></td>
+        <td id="blank"></td>
+        <td id="blank"></td>
+        <td id="blank"></td>
+        <td><button type="submit" name="checkout" class="checkoutBtn" onclick="document.location='checkout.php'">PROCEED TO CHECKOUT</button></td>
+      </tr>
+    </table>
   </div></br>
 
   <footer>
