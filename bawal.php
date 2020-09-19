21b0d3e19079b5a00720df3ac256d7ad7f239cbd
shakinassi <ohhshakina@gmail.com>
2020-09-19 14:15:15 +0800

    bawal and turban

---
+++ bawal.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+   <meta charset="utf-8">
+   <title>Bawal Collection | Scarf4u</title>
+   <link rel="stylesheet" href="bawal.css" /> 
+   <link rel="cart" href= "cart.php"</a>
+   
+</head>
+<body>
+<header>
+	<img src="images/logo.jpeg">
+</header>
+
+	<div class="alignRow">
+		<div class="imageColumn">
+		<img src="images/fairy green.jpg" alt="Fairy Green" style="width:90%">
+		<p id="name">Kekaboo – Fairy Green</p></td>
+		<p id="price">RM 25.00</p></td>
+		<button type="submit" name="bawal1" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+	
+		<div class="imageColumn">
+		<img src="images/blush pink.jpg" alt="Blush Pink"  style="width:90%">
+		<p id="name">Kekaboo – Blush Pink</p></td>
+		<p id="price">RM 25.00</p></td>
+		<button type="submit" name="bawal2" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+		<div class="imageColumn">
+		<img src="images/brownies.jpg" alt="Brownies" style="width:90%">
+		<p id="name">Kekaboo – Coffee Au Lait</p></td>
+		<p id="price">RM 25.00</p></td>
+		<button type="submit" name="bawal3" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+		<div class="imageColumn">
+		<img src="images/paddlepop.jpg" alt="Paddlepop" style="width:90%">
+		<p id="name">Kekaboo – Paddlepop</p></td>
+		<p id="price">RM 25.00</p></td>
+		<button type="submit" name="bawal4" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+		<div class="imageColumn">
+		<img src="images/puteri tiana.jpg" alt="Puteri Tiana"  style="width:90%">
+		<p id="name">Kekaboo – Puteri Tiana</p></td>
+		<p id="price">RM 25.00</p></td>
+		<button type="submit" name="bawal5" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+</div>
+
+	<div class="alignRow2">
+		<div class="imageColumn2">
+		<img src="images/safiya medina.jpg" alt="Safiya medina"  style="width:90%">
+		<p id="name">Kekaboo – Safiya Medina</p></td>
+		<p id="price">RM 25.00</p></td>
+		<button type="submit" name="bawal6" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+		<div class="imageColumn2">
+		<img src="images/malikha aura.jpg" alt="Malikha Aura"  style="width:90%">
+		<p id="name">Kekaboo – Malikha Aura</p></td>
+		<p id="price">RM 25.00</p></td>
+		<button type="submit" name="bawal7" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+		<div class="imageColumn2">
+		<img src="images/ameyrah bella.jpg" alt="Ameyrah Bella"   style="width:90%">
+		<p id="name">Kekaboo – Ameyrah Bella</p></td>
+		<p id="price">RM 25.00</p></td>
+		<button type="submit" name="bawal8" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+		<div class="imageColumn2">
+		<img src="images/zaiton aysha.jpg" alt="Zaiton Aysha"  style="width:90%">
+		<p id="name">Kekaboo – Zayton Aisya</p></td>
+		<p id="price">RM 25.00</p></td>
+		<button type="submit" name="bawal9" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+		<div class="imageColumn2">
+		<img src="images/liyana zahra.jpg" alt="Liyana Zahra"   style="width:90%">
+		<p id="name">Kekaboo – Liyana Zahra</p></td>
+		<p id="price">RM 25.00</p></td>
+		<button type="submit" name="bawal10" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+</div>
+
+	<div class="alignRow3">
+		<div class="imageColumn3">
+		<img src="images/sri-hanim.jpg" alt="Sri hanim"  style="width:90%">
+		<p id="name">Kekaboo – Mirabelle Sri Hanim</p></td>
+		<p id="price">RM 20.00</p></td>
+		<button type="submit" name="bawal11" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+		<div class="imageColumn3">
+		<img src="images/maria clara.jpg" alt="Maria Clara" style="width:90%">
+		<p id="name">Mirabelle Maria Clara</p></td>
+		<p id="price">RM 20.00</p></td>
+		<button type="submit" name="bawal12" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+		<div class="imageColumn3">
+		<img src="images/violet aurora.jpg" alt="Violet aurora" style="width:90%">
+		<p id="name">Mirabelle Violet Aurora</p></td>
+		<p id="price">RM 20.00</p></td>
+		<button type="submit" name="bawal13" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+		<div class="imageColumn3">
+		<img src="images/isobelle.jpg" alt="Isobelle" style="width:90%">
+		<p id="name">Mirabelle Isobelle</p></td>
+		<p id="price">RM 20.00</p></td>
+		<button type="submit" name="bawal14" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+		<div class="imageColumn3">
+		<img src="images/bellerose.jpg" alt="bellerose" style="width:90%">
+		<p id="name">Kekaboo – Mirabelle Bellerose</p></td>
+		<p id="price">RM 20.00</p></td>
+		<button type="submit" name="bawal15" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+</div>
+
+	<div class="alignRow4">
+		<div class="imageColumn4">
+		<img src="images/kadija.jpg" alt="Kadija"  style="width:90%">
+		<p id="name">Kekaboo – Mirabelle Kadija</p></td>
+		<p id="price">RM 20.00</p></td>
+		<button type="submit" name="bawal16" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+		<div class="imageColumn4">
+		<img src="images/louisa.jpg" alt="Louisa" style="width:90%">
+		<p id="name">Kekaboo – Mirabelle Louisa</p></td>
+		<p id="price">RM 20.00</p></td>
+		<button type="submit" name="bawal17" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+		<div class="imageColumn4">
+		<img src="images/rossaluna.jpg" alt="Rossaluna" style="width:90%">
+		<p id="name">Kekaboo – Mirabelle Rossaluna</p></td>
+		<p id="price">RM 20.00</p></td>
+		<button type="submit" name="bawal18" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+		<div class="imageColumn4">
+		<img src="images/amalea.jpg" alt="Amalea" style="width:90%">
+		<p id="name">Kekaboo – Mirabelle Amalea</p></td>
+		<p id="price">RM 20.00</p></td>
+		<button type="submit" name="bawal19" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+		<div class="imageColumn4">
+		<img src="images/violetta.jpg" alt="violetta" style="width:90%">
+		<p id="name">Kekaboo – Mirabelle Violetta</p></td>
+		<p id="price">RM 20.00</p></td>
+		<button type="submit" name="bawal20" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
+	</div>
+</div>
+
+
+<footer id="site-footer">
+
+<div id="footer1"><p>Copyright  2013 Domain Name - All Rights Reserved </p></div>
+
+</footer>
+</body>
+</html>
