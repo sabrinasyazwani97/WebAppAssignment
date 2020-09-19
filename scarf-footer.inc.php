2bbca55e9d92f5d467e35915ea7bcaedf3fc1e68
Nurul Nadiah binti Zulkifli <53247760+nadiahzulkifli98@users.noreply.github.com>
2020-09-19 21:39:37 +0800

    Add files via upload

---
+++ scarf-footer.inc.php
@@ -0,0 +1,36 @@
+<footer>
+    <div class="container">
+    
+        <div>
+            <img src="images/logo.png" alt="" style="float:left" width="380px" height="100px">
+        </div>
+    
+        <div class="ftr-customer">
+            <h4>Customer Care</h4>
+            <ul>
+                <li><a href="#">Shipping Policy</a></li>
+                <li><a href="#">Return Policy</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">About Us</a></li>
+            </ul>
+        </div>
+        
+        <div class="ftr-visit">
+            <h4>Visit Our Store</h4>
+            <p>Persiaran Multimedia</p>
+            <p>63100 Cyberjaya</p>
+            <p>Selangor</p>
+        </div>
+    
+        <div class="ftr-connected">
+            <h4>Stay Connected</h4>
+            <div class="social">
+                <div><a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a></div>
+                <div><a href="https://www.facebook.com"><i class="fab fa-facebook-f"></i></a></div>
+                <div><a href="https://www.twitter.com"><i class="fab fa-twitter"></i></a></div>
+                <div><a href="https://www.pinterest.com"><i class="fab fa-pinterest-p"></i></a></div>
+            </div>
+        </div>
+    
+    </div>
+</footer>
---
+++ scarf-header.inc.php
@@ -0,0 +1,35 @@
+<header>
+    <!-- TOP BAR -->
+    <div class="top-bar">
+        <div class="container top-bar-container">
+            <h5>LADIES TRENDS NO 1</h5>
+            <div class="vertical-line-grey"></div>
+            <a href="#">Sign Up</a>
+            <div class="vertical-line-grey"></div>
+            <a href="#">Login</a>
+            <div class="vertical-line-grey"></div>
+            <a href="cart.php"><i class="fas fa-shopping-bag"></i></a>
+        </div>
+    </div>
+    
+    <!-- MAIN NAV -->
+    <div class="main-nav-bar">
+        <div class="container main-nav-bar-container">
+            <div class="logo-container">
+                <img src="images/logo2.png" style="float:left" width="290px" height="50px"></img>
+                </span>
+            </div>
+            <nav>
+                <ul>
+                    <li><a class="active" href="#">Home</a></li>
+                    <li><a href="bawal.php">Bawal</a></li>
+                    <li><a href="turban.php">Turban</a></li>
+                    <li><a href="niqab.php">Niqab</a></li>
+                    <li><a href="shawl.php">Shawl</a></li>
+                    <li><a href="sarung.php">Sarung</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    
+</header>
