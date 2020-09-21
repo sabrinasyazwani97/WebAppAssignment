834e3eaed4315f1dbcf7175e62465bb18ff8fa94
shakina.jh <70265335+shakinassi@users.noreply.github.com>
2020-09-21 23:14:58 +0800

    Add files via upload

---
+++ turban.php
@@ -0,0 +1,103 @@
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Turban Collection | Scarf4u</title>
+  <link rel="stylesheet" href="style.css" />
+  <link href="turban.css" rel="stylesheet"/>
+  <script src="script.js"></script>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,900" rel="stylesheet">
+</head>
+
+<body>
+  <?php include 'scarf-header.inc.php';?>
+<article class="turban">
+
+  <div class="all_image">
+    <h1 id="turban-title">SARUNG COLLECTION</h1>
+    <table class="turban_table">
+      <tr>
+        <td><div class="one_image"><img src="images/FEMME-BABY-BLUE.jpg" title="Anisalidrus Turban - Turban Femme Baby Blue" id="img"></div></td>
+        <td><div class="one_image"><img src="images/FEMME-PRETTY-PINK.jpg" title="Anisalidrus Turban - Turban Femme Pretty Pink" id="img"></div></td>
+        <td><div class="one_image"><img src="images/FEMME-CHOCO-GREY.jpg" title="Anisalidrus Turban - Turban Femme Choco Grey" id="img"></div></td>
+        <td><div class="one_image"><img src="images/FEMME-MINT-GREEN.jpg" title="Anisalidrus Turban - Turban Femme Mint Green" id="img"></div></td>
+      </tr>
+      <tr>
+        <td><p id="name">Anisalidrus Turban - Turban Femme Baby Blue</p></td>
+        <td><p id="name">Anisalidrus Turban - Turban Femme Pretty Pink</p></td>
+        <td><p id="name">Anisalidrus Turban - Turban Femme Choco Grey</p></td>
+        <td><p id="name">Anisalidrus Turban - Turban Femme Mint Green</p></td>
+      </tr>
+      <tr>
+        <td><text id="price">RM75.00</text></td>
+        <td><text id="price">RM75.00</text></td>
+        <td><text id="price">RM75.00</text></td>
+        <td><text id="price">RM75.00</text></td>
+      </tr>
+      <tr>
+        <td><button type="submit" name="turban1" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="submit" name="turban2" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="submit" name="turban3" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="submit" name="turban4" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+      </tr>
+    </table></br>
+
+    <table class="turban_table">
+      <tr>
+        <td><div class="one_image"><img src="images/DENIM-BLUE.jpg" title="Anisalidrus Turban - Denim Blue" id="img"></div></td>
+        <td><div class="one_image"><img src="images/DENIM-GREY.jpg" title="Anisalidrus Turban – Denim Grey" id="img"></div></td>
+        <td><div class="one_image"><img src="images/DENIM-MUSTARD.jpg" title="Anisalidrus Turban – Denim Mustard" id="img"></div></td>
+        <td><div class="one_image"><img src="images/DENIM-GREEN.jpg" title="Anisalidrus Turban – Denim Green" id="img"></div></td>
+      </tr>
+      <tr>
+        <td><p id="name">Anisalidrus Turban - Denim Blue</p></td>
+        <td><p id="name">Anisalidrus Turban – Denim Grey</p></td>
+        <td><p id="name">Anisalidrus Turban – Denim Mustard</p></td>
+        <td><p id="name">Anisalidrus Turban – Denim Green</p></td>
+      </tr>
+      <tr>
+        <td><text id="price">RM55.00</text></td>
+        <td><text id="price">RM55.00</text></td>
+        <td><text id="price">RM55.00</text></td>
+        <td><text id="price">RM55.00</text></td>
+      </tr>
+      <tr>
+        <td><button type="submit" name="turban5" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="submit" name="turban6" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="submit" name="turban7" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="submit" name="turban8" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+      </tr>
+    </table></br>
+
+    <table class="turban_table">
+      <tr>
+        <td><div class="one_image"><img src="images/DENIM-PINK.jpg" title="Anisalidrus Turban - Denim Pink" id="img"></div></td>
+        <td><div class="one_image"><img src="images/BASIC-BLUE.jpg" title="Anisalidrus Turban – Basic Blue" id="img"></div></td>
+        <td><div class="one_image"><img src="images/BASIC-WHITE.jpg" title="Anisalidrus Turban – Basic White" id="img"></div></td>
+      </tr>
+      <tr>
+        <td><p id="name">Anisalidrus Turban - Denim Pink</p></td>
+        <td><p id="name">Anisalidrus Turban – Basic Blue</p></td>
+        <td><p id="name">Anisalidrus Turban – Basic White</p></td>
+      </tr>
+      <tr>
+        <td><text id="price">RM55.00</text></td>
+        <td><text id="price">RM50.00</text></td>
+        <td><text id="price">RM50.00</text></td>
+      </tr>
+      <tr>
+        <td><button type="submit" name="turban9" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="submit" name="turban10" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+        <td><button type="submit" name="turban11" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+      </tr>
+    </table></br>
+
+  
+
+   
+  </div>
+</article>
+  <?php include 'scarf-footer.inc.php';?>
+</body>
+</html>
+
