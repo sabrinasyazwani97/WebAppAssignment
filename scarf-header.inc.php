da0bc0a5d27187173269cb21c22800d514d0d2bd
Nurul Nadiah binti Zulkifli <53247760+nadiahzulkifli98@users.noreply.github.com>
2020-09-21 12:37:30 +0800

    Update scarf-header.inc.php

--- scarf-header.inc.php
+++ scarf-header.inc.php
@@ -4,9 +4,9 @@
         <div class="container top-bar-container">
             <h5>LADIES TRENDS NO 1</h5>
             <div class="vertical-line-grey"></div>
-            <a href="#">Sign Up</a>
+            <a href="register.php">Sign Up</a>
             <div class="vertical-line-grey"></div>
-            <a href="#">Login</a>
+            <a href="login.php">Login</a>
             <div class="vertical-line-grey"></div>
             <a href="cart.php"><i class="fas fa-shopping-bag"></i></a>
         </div>
@@ -21,7 +21,7 @@
             </div>
             <nav>
                 <ul>
-                    <li><a class="active" href="#">Home</a></li>
+                    <li><a href="home.php">Home</a></li>
                     <li><a href="bawal.php">Bawal</a></li>
                     <li><a href="turban.php">Turban</a></li>
                     <li><a href="niqab.php">Niqab</a></li>
