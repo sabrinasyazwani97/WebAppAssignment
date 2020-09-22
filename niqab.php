ea49dc78c0402496d1907b99b8c668838e593c4c
alyaamn <71474184+alyaamn@users.noreply.github.com>
2020-09-22 18:26:59 +0800

    Update niqab.php

--- niqab.php
+++ niqab.php
@@ -1,142 +1,129 @@
-<!DOCTYPE html>
-
 <html>
 	<head lang = "en">
 		<meta charset = "utf-8">
-		<title>Niqab | Scarf4u</title>
-		<link rel="stylesheet" href="niqab.css" />
-		<link rel="cart" href "cart.php"</a>
+		<title>Niqab Colection | Scarf4u</title>
+		<link rel="stylesheet" href="style.css" />
+		<link href="niqab.css" rel="stylesheet"/>
+		<script src="script.js"></script>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,900" rel="stylesheet">
 	</head>
 	
 	<body>
-		<header>
-			<h1 class = "title">NIQAB</h1>
-		</header>
+		<?php include 'scarf-header.inc.php';?>
+		<article class="niqab">
 		
-		<div>
-			<div class = "niqab">
-				<div class = "row">
-					<div class="col">
-						<img src= "photo/belani-black.jpg">
-						<h3>Niqab Belani - Black</h3>
-						<p>RM20.00</p>
-						<button type="submit" name="niqab1" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/belani-brown.jpg">
-						<h3>Niqab Belani - Brown</h3>
-						<p>RM20.00</p>
-						<button type="submit" name="niqab2" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/belani-green.jpg">
-						<h3>Niqab Belani - Emerald Green</h3>
-						<p>RM20.00</p>
-						<button type="submit" name="niqab3" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/belani-pink.jpg">
-						<h3>Niqab Belani - Charmine Pink</h3>
-						<p>RM20.00</p>
-						<button type="submit" name="niqab4" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-				</div>
+			<div class="all_image">
+				<h1 id="niqab-title">NIQAB COLLECTION</h1>
+				<table class="niqab_table">
+				<tr>
+					<td><div class="one_image"><img src="Image/Niqab/belani-black.jpg" title="Niqab Belani - Black" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Niqab/belani-brown.jpg" title="Niqab Belani - Brown" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Niqab/belani-green.jpg" title="Niqab Belani - Emerald Green" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Niqab/belani-pink.jpg" title="Niqab Belani - Charmine Pink" id="img"></div></td>
+				</tr>		
+				<tr>
+					<td><p id="name">Niqab Belani - Black</p></td>
+					<td><p id="name">Niqab Belani - Brown</p></td>
+					<td><p id="name">Niqab Belani - Emerald Green</p></td>
+					<td><p id="name">Niqab Belani - Charmine Pink</p></td>
+				</tr>				
+				<tr>
+					<td><text id="price">RM20.00</text></td>
+					<td><text id="price">RM20.00</text></td>
+					<td><text id="price">RM20.00</text></td>
+					<td><text id="price">RM20.00</text></td>
+				</tr>				
+				<tr>
+					<td><button type="submit" name="niqab1" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="niqab2" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="niqab3" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="niqab4" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+				</tr>
+				</table></br>
+				
+				<table class="niqab_table">
+				<tr>
+					<td><div class="one_image"><img src="Image/Niqab/melani-darkblue.jpg" title="Niqab Melani - Dark Blue" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Niqab/melani-maroon.jpg" title="Niqab Melani - Maroon" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Niqab/khimar-black.jpg" title="Niqab Khimar - Black" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Niqab/khimar-white_black.jpg" title="Niqab Khimar - White & Black" id="img"></div></td>
+				</tr>				
+				<tr>
+					<td><p id="name">Niqab Melani - Dark Blue</p></td>
+					<td><p id="name">Niqab Melani - Maroon</p></td>
+					<td><p id="name">Niqab Khimar - Black</p></td>
+					<td><p id="name">Niqab Khimar - White & Black</p></td>
+				</tr>				
+				<tr>
+					<td><text id="price">RM35.00</text></td>
+					<td><text id="price">RM35.00</text></td>
+					<td><text id="price">RM69.00</text></td>
+					<td><text id="price">RM69.00</text></td>
+				</tr>				
+				<tr>
+					<td><button type="submit" name="niqab5" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="niqab6" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="niqab7" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="niqab8" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+				</tr>
+				</table></br>
 
-				<div class = "row">
-					<div class="col">
-						<img src= "photo/melani-darkblue.jpg">
-						<h3>Niqab Melani - Dark Blue</h3>
-						<p>RM35.00</p>
-						<button type="submit" name="niqab5" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/melani-maroon.jpg">
-						<h3>Niqab Melani - Maroon</h3>
-						<p>RM35.00</p>
-						<button type="submit" name="niqab6" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-				
-					<div class="col">
-						<img src= "photo/khimar-black.jpg">
-						<h3>Niqab Khimar - Black</h3>
-						<p>RM69.00</p>
-						<button type="submit" name="niqab7" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/khimar-white_black.jpg">
-						<h3>Niqab Khimar - White & Black</h3>
-						<p>RM69.00</p>
-						<button type="submit" name="niqab8" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-				</div>
-					
-				<div class = "row">
-					<div class="col">
-						<img src= "photo/khimar-sumayyah.jpg">
-						<h3>Niqab Khimar (Sumayyah) - Black</h3>
-						<p>RM69.00</p>
-						<button type="submit" name="niqab9" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/khimar-sara4xl.jpg">
-						<h3>Niqab Khimar (Sara 4XL) - Black</h3>
-						<p>RM99.00</p>
-						<button type="submit" name="niqab10" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-				
-					<div class="col">
-						<img src= "photo/khimar_sara4xl-latte.jpg">
-						<h3>Niqab Khimar (Sara 4XL) - Latte</h3>
-						<p>RM99.00</p>
-						<button type="submit" name="niqab11" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/sara-black.jpg">
-						<h3>Niqab Sara - Black</h3>
-						<p>RM39.00</p>
-						<button type="submit" name="niqab12" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-				</div>
-				
-				<div class = "row">
-					<div class="col">
-						<img src= "photo/fiona.jpg">
-						<h3>Niqab Fiona - Black</h3>
-						<p>RM55.00</p>
-						<button type="submit" name="niqab13" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/laora-black.jpg">
-						<h3>Niqab Laora - Black</h3>
-						<p>RM69.00</p>
-						<button type="submit" name="niqab14" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/layla-black.jpg">
-						<h3>Niqab Layla - Black</h3>
-						<p>RM39.00</p>
-						<button type="submit" name="niqab15" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-						
-					<div class="col">
-						<img src= "photo/sofya-black.jpg">
-						<h3>Niqab Sofya - Black</h3>
-						<p>RM65.00</p>
-						<button type="submit" name="niqab16" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>					
-				</div>
+				<table class="niqab_table">
+				<tr>
+					<td><div class="one_image"><img src="Image/Niqab/khimar-sumayyah.jpg" title="Niqab Khimar (Sumayyah) - Black" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Niqab/khimar-sara4xl.jpg" title="Niqab Khimar (Sara 4XL) - Black" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Niqab/khimar-sara4xl.jpg" title="Niqab Khimar (Sara 4XL) - Latte" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Niqab/sara-black.jpg" title="Niqab Sara - Black" id="img"></div></td>
+				</tr>
+				<tr>
+					<td><p id="name">Niqab Khimar (Sumayyah) - Black</p></td>
+					<td><p id="name">Niqab Khimar (Sara 4XL) - Black</p></td>
+					<td><p id="name">Niqab Khimar (Sara 4XL) - Latte</p></td>
+					<td><p id="name">Niqab Sara - Black</p></td>
+				</tr>				
+				<tr>
+					<td><text id="price">RM69.00</text></td>
+					<td><text id="price">RM99.00</text></td>
+					<td><text id="price">RM99.00</text></td>
+					<td><text id="price">RM39.00</text></td>
+				</tr>				
+				<tr>
+					<td><button type="submit" name="niqab9" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="niqab10" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="niqab11" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="niqab12" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+				</tr>
+				</table><br>
+
+				<table class="niqab_table">
+				<tr>
+					<td><div class="one_image"><img src="Image/Niqab/fiona.jpg" title="Niqab Fiona - Black" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Niqab/laora-black.jpg" title="Niqab Laora - Black" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Niqab/layla-black.jpg" title="NNiqab Layla - Black" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Niqab/sofya-black.jpg" title="Niqab Sofya - Black" id="img"></div></td>
+				</tr>
+				<tr>
+					<td><p id="name">Niqab Fiona - Black</p></td>
+					<td><p id="name">Niqab Laora - Black</p></td>
+					<td><p id="name">Niqab Layla - Black</p></td>
+					<td><p id="name">Niqab Sofya - Black</p></td>
+				</tr>				
+				<tr>
+					<td><text id="price">RM55.00</text></td>
+					<td><text id="price">RM69.00</text></td>
+					<td><text id="price">RM39.00</text></td>
+					<td><text id="price">RM65.00</text></td>
+				</tr>				
+				<tr>
+					<td><button type="submit" name="niqab13" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="niqab14" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="niqab15" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="niqab16" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+				</tr>
+				</table></br>				
 			</div>
-		</div>
-		
+		</article>
+		<?php include 'scarf-footer.inc.php';?>
 	</body>
-
 </html>
