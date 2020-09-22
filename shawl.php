4e94753cce50acdcfb8f68ec9d1a181750759537
alyaamn <71474184+alyaamn@users.noreply.github.com>
2020-09-22 18:25:50 +0800

    Update shawl.php

--- shawl.php
+++ shawl.php
@@ -1,156 +1,153 @@
-<!DOCTYPE html>
-
 <html>
-	<head>
-		<title>Shawl | Scarf4u</title>
-		<link rel="stylesheet" href="shawl.css" />
+	<head lang = "en">
+		<meta charset = "utf-8">
+		<title>Shawl Colection | Scarf4u</title>
+		<link rel="stylesheet" href="style.css" />
+		<link href="shawl.css" rel="stylesheet"/>
+		<script src="script.js"></script>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,900" rel="stylesheet">
 	</head>
 	
 	<body>
-		<header>
-			<h1 class = "title">SHAWL</h1>
-		</header>
+		<?php include 'scarf-header.inc.php';?>
+		<article class="shawl">
 		
-		<div>
-			<div class = "shawl">
-				<div class = "row">
-					<div class="col">
-						<img src= "photo/naelofar_eva-candypink.jpg">
-						<h4>Naelofar</h4>
-						<h3>Instant Shawl Eva - Candy Pink</h3>
-						<p>RM79.00</p>
-						<button type="submit" name="shawl1" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/naelofar_eva-ambrosia.jpg">
-						<h4>Naelofar</h4>
-						<h3>Instant Shawl Eva - Ambrosia</h3>
-						<p>RM79.00</p>
-						<button type="submit" name="shawl2" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/lady_semi-plainblue.jpg">
-						<h4>Lady Arrisa Couture</h4>
-						<h3>Semi Instant Hijab <br> Audrey Exclusive - Plain Blue</h3>
-						<p>RM129.90</p>
-						<button type="submit" name="shawl3" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/lady_semi-plaingreen.jpg">
-						<h4>Lady Arrisa Couture</h4>
-						<h3>Semi Instant Hijab <br> Audrey Exclusive - Plain Green</h3>
-						<p>RM129.90</p>
-						<button type="submit" name="shawl4" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-				</div>
+			<div class="all_image">
+				<h1 id="shawl-title">SHAWL COLLECTION</h1>
+				<table class="shawl_table">
+				<tr>
+					<td><div class="one_image"><img src="Image/Shawl/naelofar_eva-candypink.jpg" title="NAELOFAR Instant Shawl Eva - Candy Pink" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Shawl/naelofar_eva-ambrosia.jpg" title="NAELOFAR Instant Shawl Eva - Ambrosia" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Shawl/lady_semi-plainblue.jpg" title="LADY ARISSA COUTURE Semi Instant Hijab - Plain Blue" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Shawl/lady_semi-plaingreen.jpg" title="LADY ARISSA COUTURE Semi Instant Hijab - Plain Green" id="img"></div></td>
+				</tr>				
+				<tr>
+					<td><p id="brand">NAELOFAR</p></td>
+					<td><p id="brand">NAELOFAR</p></td>
+					<td><p id="brand">LADY ARISSA COUTURE</p></td>
+					<td><p id="brand">LADY ARISSA COUTURE</p></td>
+				</tr>				
+				<tr>
+					<td><p id="name">Instant Shawl Eva - Candy Pink</p></td>
+					<td><p id="name">Instant Shawl Eva - Ambrosia</p></td>
+					<td><p id="name">Semi Instant Hijab Audrey Exclusive - Plain Blue</p></td>
+					<td><p id="name">Semi Instant Hijab Audrey Exclusive - Plain Green</p></td>
+				</tr>				
+				<tr>
+					<td><text id="price">RM79.00</text></td>
+					<td><text id="price">RM79.00</text></td>
+					<td><text id="price">RM129.90</text></td>
+					<td><text id="price">RM129.90</text></td>
+				</tr>				
+				<tr>
+					<td><button type="submit" name="shawl1" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="shawl2" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="shawl3" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="shawl4" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+				</tr>
+				</table></br>
+				
+				<table class="shawl_table">
+				<tr>
+					<td><div class="one_image"><img src="Image/Shawl/rayzara_alyah-purple.jpg" title="RAYZARA Instant Shawl Alyah - Purple" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Shawl/rayzara_alyah-red.jpg" title="RAYZARA Instant Shawl Alyah - Red" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Shawl/rayzara_shayla-purple.jpg" title="RAYZARA Satin Shawl Shayla - Purple" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Shawl/rayzara_shayla-turquoise.jpg" title="RAYZARA Satin Shawl Shayla - Turquoise" id="img"></div></td>
+				</tr>				
+				<tr>
+					<td><p id="brand">RAYZARA</p></td>
+					<td><p id="brand">RAYZARA</p></td>
+					<td><p id="brand">RAYZARA</p></td>
+					<td><p id="brand">RAYZARA</p></td>
+				</tr>				
+				<tr>
+					<td><p id="name">Instant Shawl Alyah - Purple</p></td>
+					<td><p id="name">Instant Shawl Alyah - Red</p></td>
+					<td><p id="name">Satin Shawl Shayla - Purple</p></td>
+					<td><p id="name">Satin Shawl Shayla - Turquoise</p></td>
+				</tr>				
+				<tr>
+					<td><text id="price">RM55.00</text></td>
+					<td><text id="price">RM55.00</text></td>
+					<td><text id="price">RM39.00</text></td>
+					<td><text id="price">RM39.00</text></td>
+				</tr>				
+				<tr>
+					<td><button type="submit" name="shawl5" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="shawl6" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="shawl7" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="shawl8" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+				</tr>
+				</table></br>
 
-				<div class = "row">
-					<div class="col">
-						<img src= "photo/rayzara_alyah-purple.jpg">
-						<h4>Rayzara</h4>
-						<h3>Instant Shawl Alyah - Purple</h3>
-						<p>RM55.00</p>
-						<button type="submit" name="shawl5" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/rayzara_alyah-red.jpg">
-						<h4>Rayzara</h4>
-						<h3>Instant Shawl Alyah - Red</h3>
-						<p>RM55.00</p>
-						<button type="submit" name="shawl6" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-				
-					<div class="col">
-						<img src= "photo/rayzara_shayla-purple.jpg">
-						<h4>Rayzara</h4>
-						<h3>Satin Shawl Shayla - Purple</h3>
-						<p>RM39.00</p>
-						<button type="submit" name="shawl7" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/rayzara_shayla-turquoise.jpg">
-						<h4>Rayzara</h4>
-						<h3>Satin Shawl Shayla - Turquoise</h3>
-						<p>RM39.00</p>
-						<button type="submit" name="shawl8" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-				</div>
-					
-				<div class = "row">
-					<div class="col">
-						<img src= "photo/rayzara-instant_shawl-black.jpg">
-						<h4>Rayzara</h4>
-						<h3>Instant Shawl - Black</h3>
-						<p>RM35.00</p>
-						<button type="submit" name="shawl9" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/rayzara-instant_shawl-red.jpg">
-						<h4>Rayzara</h4>
-						<h3>Instant Shawl - Red Chilli</h3>
-						<p>RM35.00</p>
-						<button type="submit" name="shawl10" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-				
-					<div class="col">
-						<img src= "photo/meylana-scarf-flowers.jpg">
-						<h4>Meylana</h4>
-						<h3>Square Scarf Flowers - Purple</h3>
-						<p>RM89.00</p>
-						<button type="submit" name="shawl11" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/meylana-scarf-flowers1.jpg">
-						<h4>Meylana</h4>
-						<h3>Square Scarf Flowers - Peach</h3>
-						<p>RM89.00</p>
-						<button type="submit" name="shawl12" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-				</div>
-				
-				<div class = "row">
-					<div class="col">
-						<img src= "photo/adara-ozel_collection_egypt.jpg">
-						<h4>Adara</h4>
-						<h3>Ozel Collection Eqypt</h3>
-						<p>RM159.00</p>
-						<button type="submit" name="shawl13" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/adara-ozel_collection_qatar.jpg">
-						<h4>Adara</h4>
-						<h3>Ozel Collection Qatar</h3>
-						<p>RM159.00</p>
-						<button type="submit" name="shawl14" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-					
-					<div class="col">
-						<img src= "photo/fateema_dream-masonblue.jpg">
-						<h4>Fateema Couture</h4>
-						<h3>Shawl Dream Catcher - Mason Blue</h3>
-						<p>RM49.00</p>
-						<button type="submit" name="shawl15" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>
-						
-					<div class="col">
-						<img src= "photo/fateema_dream-mustard.jpg">
-						<h4>Fateema Couture</h4>
-						<h3>Shawl Dream Catcher - Mustard</h3>
-						<p>RM49.00</p>
-						<button type="submit" name="shawl16" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button>
-					</div>					
-				</div>
+				<table class="shawl_table">
+				<tr>
+					<td><div class="one_image"><img src="Image/Shawl/rayzara-instant_shawl-black.jpg" title="RAYZARA Instant Shawl - Black" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Shawl/rayzara-instant_shawl-red.jpg" title="RAYZARA Instant Shawl - Red Chilli" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Shawl/meylana-scarf-flowers.jpg" title="MEYLANA Square Scarf Flowers - Purple" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Shawl/meylana-scarf-flowers1.jpg" title="MEYLANA Square Scarf Flowers - Peach" id="img"></div></td>
+				</tr>				
+				<tr>
+					<td><p id="brand">RAYZARA</p></td>
+					<td><p id="brand">RAYZARA</p></td>
+					<td><p id="brand">MEYLANA</p></td>
+					<td><p id="brand">MEYLANA</p></td>
+				</tr>				
+				<tr>
+					<td><p id="name">Instant Shawl - Black</p></td>
+					<td><p id="name">Instant Shawl - Red Chilli</p></td>
+					<td><p id="name">Square Scarf Flowers - Purple</p></td>
+					<td><p id="name">Square Scarf Flowers - Peach</p></td>
+				</tr>				
+				<tr>
+					<td><text id="price">RM35.00</text></td>
+					<td><text id="price">RM35.00</text></td>
+					<td><text id="price">RM89.00</text></td>
+					<td><text id="price">RM89.00</text></td>
+				</tr>				
+				<tr>
+					<td><button type="submit" name="shawl9" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="shawl10" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="shawl11" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="shawl12" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+				</tr>
+				</table><br>
+
+				<table class="shawl_table">
+				<tr>
+					<td><div class="one_image"><img src="Image/Shawl/adara-ozel_collection_egypt.jpg" title="Ozel Collection Egypt" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Shawl/adara-ozel_collection_qatar.jpg" title="Ozel Collection Qatar" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Shawl/fateema_dream-masonblue.jpg" title="Shawl Dream Catcher - Mason Blue" id="img"></div></td>
+					<td><div class="one_image"><img src="Image/Shawl/fateema_dream-mustard.jpg" title="Shawl Dream Catcher - Mustard" id="img"></div></td>
+				</tr>
+				<tr>
+					<td><p id="brand">ADARA</p></td>
+					<td><p id="brand">ADARA</p></td>
+					<td><p id="brand">FATEEMA COUTURE</p></td>
+					<td><p id="brand">FATEEMA COUTURE</p></td>
+				</tr>
+				<tr>
+					<td><p id="name">Ozel Collection Egypt</p></td>
+					<td><p id="name">Ozel Collection Qatar</p></td>
+					<td><p id="name">Shawl Dream Catcher - Mason Blue</p></td>
+					<td><p id="name">Shawl Dream Catcher - Mustard</p></td>
+				</tr>
+				<tr>
+					<td><text id="price">RM159.00</text></td>
+					<td><text id="price">RM159.00</text></td>
+					<td><text id="price">RM49.00</text></td>
+					<td><text id="price">RM49.00</text></td>
+				</tr>
+				<tr>
+					<td><button type="submit" name="shawl13" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="shawl14" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="shawl15" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+					<td><button type="submit" name="shawl16" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
+				</tr>
+				</table></br>				
 			</div>
-		</div>
-	
+		</article>
+		<?php include 'scarf-footer.inc.php';?>
 	</body>
-
 </html>
