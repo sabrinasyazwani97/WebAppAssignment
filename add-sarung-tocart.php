38e8e137d3bae086ddef600dd4c048c21f1ba35e
Sabrina Mazlan <54005211+sabrinasyazwani97@users.noreply.github.com>
2020-09-28 12:57:12 +0800

    Update and rename add-items.php to add-sarung-tocart.php

--- add-sarung-tocart.php
+++ add-sarung-tocart.php
@@ -3,7 +3,9 @@
     $database_name = "shoppingcart";
     $con = mysqli_connect("localhost","root","",$database_name);
 
+    //add to cart
     if (isset($_POST["add"])){
+
         if (isset($_SESSION["cart"])){
             $item_array_id = array_column($_SESSION["cart"],"product_id");
             if (!in_array($_GET["id"],$item_array_id)){
@@ -13,13 +15,13 @@
                     'item_name' => $_POST["hidden_name"],
                     'product_price' => $_POST["hidden_price"],
                     'item_quantity' => $_POST["quantity"],
-                    'item_image' => $_POST["image"],
+                    'item_image' => $_POST["hidden_img"],
                 );
                 $_SESSION["cart"][$count] = $item_array;
-                echo '<script>window.location="cart.php"</script>';
+                echo '<script>window.location="sarung.php"</script>';
             }else{
                 echo '<script>alert("Product is already Added to Cart")</script>';
-                echo '<script>window.location="cart.php"</script>';
+                echo '<script>window.location="sarung.php"</script>';
             }
         }else{
             $item_array = array(
@@ -27,12 +29,14 @@
                 'item_name' => $_POST["hidden_name"],
                 'product_price' => $_POST["hidden_price"],
                 'item_quantity' => $_POST["quantity"],
-                'item_image' => $_GET["image"],
+                'item_image' => $_POST["hidden_img"],
             );
             $_SESSION["cart"][0] = $item_array;
         }
     }
 
+
+    //delete
     if (isset($_GET["action"])){
         if ($_GET["action"] == "delete"){
             foreach ($_SESSION["cart"] as $keys => $value){
@@ -44,4 +48,29 @@
             }
         }
     }
+
+
+
+
+    //add 1 item
+    if (isset($_POST["update"])){
+        if (isset($_SESSION["cart"])){
+            $item_array_id = array_column($_SESSION["cart"],"quantity");
+            if (!in_array($_GET["quantity"],$item_array_id)){
+                $count = count($_SESSION["cart"]);
+                $item_array = array(
+                    'product_id' => $_POST["id"],
+                    'item_name' => $_POST["hidden_name"],
+                    'product_price' => $_POST["hidden_price"],
+                    'item_quantity' => $_GET["quantity"],
+                    'item_image' => $_POST["hidden_img"],
+                );
+                $_SESSION["cart"][$count] = $item_array;
+                echo '<script>window.location="cart.php"</script>';
+            }else{
+                echo '<script>alert("Product is added")</script>';
+                echo '<script>window.location="cart.php"</script>';
+            }
+        }
+      }
 ?>
