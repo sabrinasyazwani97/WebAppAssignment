b58f1e1b2f425a89df4386477da3fb9472d1217a
Nurul Nadiah binti Zulkifli <53247760+nadiahzulkifli98@users.noreply.github.com>
2020-09-29 14:00:15 +0800

    Update home.php

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-<link rel="stylesheet" href="style.css" />
+<link rel="stylesheet" href="css/style.css" />
 <script src="script.js"></script>
 
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
