d7a9828333a6df369d42fa2ac75eac49e8f08f4e
alyaamn <71474184+alyaamn@users.noreply.github.com>
2020-09-29 18:09:08 +0800

    Update orders.sql

--- db/orders.sql
+++ db/orders.sql
@@ -1,3 +1,10 @@
 --
 -- Table structure for table `orders`
 --
+CREATE TABLE `orders` (
+	`order_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`user_id` int(11) NOT NULL,
+	`quantity_product` int(11) NOT NULL,
+	`total` decimal(6,2) NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES USERS(user_id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
