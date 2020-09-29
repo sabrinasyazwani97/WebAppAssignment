40e718133f1ca50270acd86b6915389933b87ae8
alyaamn <71474184+alyaamn@users.noreply.github.com>
2020-09-29 19:09:09 +0800

    Update niqab.php

--- niqab.php
+++ niqab.php
@@ -1,3 +1,5 @@
+<?php include 'add-niqab-tocart.php';?>
+
 <html>
 	<head lang = "en">
 		<meta charset = "utf-8">
@@ -12,117 +14,47 @@
 	<body>
 		<?php include 'scarf-header.inc.php';?>
 		<article class="niqab">
+			<h1 id="niqab-title">NIQAB COLLECTION</h1>
 		
 			<div class="all_image">
-				<h1 id="niqab-title">NIQAB COLLECTION</h1>
-				<table class="niqab_table">
-				<tr>
-					<td><div class="one_image"><img src="Image/Niqab/belani-black.jpg" title="Niqab Belani - Black" id="img"></div></td>
-					<td><div class="one_image"><img src="Image/Niqab/belani-brown.jpg" title="Niqab Belani - Brown" id="img"></div></td>
-					<td><div class="one_image"><img src="Image/Niqab/belani-green.jpg" title="Niqab Belani - Emerald Green" id="img"></div></td>
-					<td><div class="one_image"><img src="Image/Niqab/belani-pink.jpg" title="Niqab Belani - Charmine Pink" id="img"></div></td>
-				</tr>		
-				<tr>
-					<td><p id="name">Niqab Belani - Black</p></td>
-					<td><p id="name">Niqab Belani - Brown</p></td>
-					<td><p id="name">Niqab Belani - Emerald Green</p></td>
-					<td><p id="name">Niqab Belani - Charmine Pink</p></td>
-				</tr>				
-				<tr>
-					<td><text id="price">RM20.00</text></td>
-					<td><text id="price">RM20.00</text></td>
-					<td><text id="price">RM20.00</text></td>
-					<td><text id="price">RM20.00</text></td>
-				</tr>				
-				<tr>
-					<td><button type="submit" name="niqab1" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-					<td><button type="submit" name="niqab2" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-					<td><button type="submit" name="niqab3" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-					<td><button type="submit" name="niqab4" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-				</tr>
-				</table></br>
-				
-				<table class="niqab_table">
-				<tr>
-					<td><div class="one_image"><img src="Image/Niqab/melani-darkblue.jpg" title="Niqab Melani - Dark Blue" id="img"></div></td>
-					<td><div class="one_image"><img src="Image/Niqab/melani-maroon.jpg" title="Niqab Melani - Maroon" id="img"></div></td>
-					<td><div class="one_image"><img src="Image/Niqab/khimar-black.jpg" title="Niqab Khimar - Black" id="img"></div></td>
-					<td><div class="one_image"><img src="Image/Niqab/khimar-white_black.jpg" title="Niqab Khimar - White & Black" id="img"></div></td>
-				</tr>				
-				<tr>
-					<td><p id="name">Niqab Melani - Dark Blue</p></td>
-					<td><p id="name">Niqab Melani - Maroon</p></td>
-					<td><p id="name">Niqab Khimar - Black</p></td>
-					<td><p id="name">Niqab Khimar - White & Black</p></td>
-				</tr>				
-				<tr>
-					<td><text id="price">RM35.00</text></td>
-					<td><text id="price">RM35.00</text></td>
-					<td><text id="price">RM69.00</text></td>
-					<td><text id="price">RM69.00</text></td>
-				</tr>				
-				<tr>
-					<td><button type="submit" name="niqab5" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-					<td><button type="submit" name="niqab6" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-					<td><button type="submit" name="niqab7" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-					<td><button type="submit" name="niqab8" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-				</tr>
-				</table></br>
 
-				<table class="niqab_table">
-				<tr>
-					<td><div class="one_image"><img src="Image/Niqab/khimar-sumayyah.jpg" title="Niqab Khimar (Sumayyah) - Black" id="img"></div></td>
-					<td><div class="one_image"><img src="Image/Niqab/khimar-sara4xl.jpg" title="Niqab Khimar (Sara 4XL) - Black" id="img"></div></td>
-					<td><div class="one_image"><img src="Image/Niqab/khimar_sara4xl-latte.jpg" title="Niqab Khimar (Sara 4XL) - Latte" id="img"></div></td>
-					<td><div class="one_image"><img src="Image/Niqab/sara-black.jpg" title="Niqab Sara - Black" id="img"></div></td>
-				</tr>
-				<tr>
-					<td><p id="name">Niqab Khimar (Sumayyah) - Black</p></td>
-					<td><p id="name">Niqab Khimar (Sara 4XL) - Black</p></td>
-					<td><p id="name">Niqab Khimar (Sara 4XL) - Latte</p></td>
-					<td><p id="name">Niqab Sara - Black</p></td>
-				</tr>				
-				<tr>
-					<td><text id="price">RM69.00</text></td>
-					<td><text id="price">RM99.00</text></td>
-					<td><text id="price">RM99.00</text></td>
-					<td><text id="price">RM39.00</text></td>
-				</tr>				
-				<tr>
-					<td><button type="submit" name="niqab9" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-					<td><button type="submit" name="niqab10" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-					<td><button type="submit" name="niqab11" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-					<td><button type="submit" name="niqab12" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-				</tr>
-				</table><br>
+			<?php
+			  $query = "SELECT * FROM products WHERE category = 'Niqab' ORDER BY id ASC";
+			  $result = mysqli_query($con,$query);
+			  if(mysqli_num_rows($result) > 0) {
 
-				<table class="niqab_table">
-				<tr>
-					<td><div class="one_image"><img src="Image/Niqab/fiona.jpg" title="Niqab Fiona - Black" id="img"></div></td>
-					<td><div class="one_image"><img src="Image/Niqab/laora-black.jpg" title="Niqab Laora - Black" id="img"></div></td>
-					<td><div class="one_image"><img src="Image/Niqab/layla-black.jpg" title="NNiqab Layla - Black" id="img"></div></td>
-					<td><div class="one_image"><img src="Image/Niqab/sofya-black.jpg" title="Niqab Sofya - Black" id="img"></div></td>
-				</tr>
-				<tr>
-					<td><p id="name">Niqab Fiona - Black</p></td>
-					<td><p id="name">Niqab Laora - Black</p></td>
-					<td><p id="name">Niqab Layla - Black</p></td>
-					<td><p id="name">Niqab Sofya - Black</p></td>
-				</tr>				
-				<tr>
-					<td><text id="price">RM55.00</text></td>
-					<td><text id="price">RM69.00</text></td>
-					<td><text id="price">RM39.00</text></td>
-					<td><text id="price">RM65.00</text></td>
-				</tr>				
-				<tr>
-					<td><button type="submit" name="niqab13" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-					<td><button type="submit" name="niqab14" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-					<td><button type="submit" name="niqab15" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-					<td><button type="submit" name="niqab16" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-				</tr>
-				</table></br>				
-			</div>
+				  while ($row = mysqli_fetch_array($result)) {
+
+					  ?>
+					  <form method="post" action="niqab.php?action=add&id=<?php echo $row["id"]; ?>">
+						<div>
+						<table class="niqab_table">
+							<tr>
+							  <td id="row-img"><div class="one_image"><img src="<?php echo $row["image"]; ?>" title="<?php echo $row["name"]; ?>" id="img"></div></td>
+							</tr>
+							<tr>
+							  <td id="row-name"><p id="name"><?php echo $row["name"]; ?></p></td>
+							</tr>
+							<tr>
+							  <td id="row-price"><text id="price"><?php echo 'RM'.$row["price"]; ?></text></td>
+							</tr>
+							<input type="hidden" name="quantity" value="1";>
+							<input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>">
+							<input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>">
+							<input type="hidden" name="hidden_img" value="<?php echo $row["image"]; ?>">
+							<tr>
+							  <td><button type="submit" name="add" class="addCartBtn">ADD TO CART</button></td>
+							</tr>
+							</table>
+						  </div>
+					  </form>
+					  <?php
+				  }
+			  }
+		  ?>
+		  </br>
+		</div>
+
 		</article>
 		<?php include 'scarf-footer.inc.php';?>
 	</body>
