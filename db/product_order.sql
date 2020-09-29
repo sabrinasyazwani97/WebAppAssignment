1d597f5c73a91b7dd4e55048add434494d7c82dd
alyaamn <71474184+alyaamn@users.noreply.github.com>
2020-09-29 18:11:33 +0800

    Update product_order.sql

--- db/product_order.sql
+++ db/product_order.sql
@@ -1,3 +1,12 @@
 --
 -- Table structure for table `product_order`
 --
+CREATE TABLE `product_order` (
+	`po_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`product_id` int(8) NOT NULL,
+	`user_id` int(11) NOT NULL,
+	`quantity` int(11) NOT NULL,
+	`subtotal` decimal(6,2) NOT NULL,
+	FOREIGN KEY (product_id) REFERENCES PRODUCTS(product_id),
+	FOREIGN KEy (user_id) REFERENCES USERS(user_id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
