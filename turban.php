a5f3c900add9804caeba7d366c8972f6ed68f352
Nur Shakina <70265335+shakinassi@users.noreply.github.com>
2020-10-03 16:36:08 +0800

    Update turban.php

--- turban.php
+++ turban.php
@@ -1,9 +1,11 @@
+<?php include 'add-turban-tocart.php';?>
+
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <title>Turban Collection | Scarf4u</title>
-  <link rel="stylesheet" href="style.css" />
-  <link href="turban.css" rel="stylesheet"/>
+  <link rel="stylesheet" href="css/style.css" />
+  <link href="css/turban.css" rel="stylesheet"/>
   <script src="script.js"></script>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,900" rel="stylesheet">
@@ -11,93 +13,49 @@
 
 <body>
   <?php include 'scarf-header.inc.php';?>
-<article class="turban">
-
-  <div class="all_image">
+  <article class="turban">
     <h1 id="turban-title">TURBAN COLLECTION</h1>
-    <table class="turban_table">
-      <tr>
-        <td><div class="one_image"><img src="images/FEMME-BABY-BLUE.jpg" title="Anisalidrus Turban - Turban Femme Baby Blue" id="img"></div></td>
-        <td><div class="one_image"><img src="images/FEMME-PRETTY-PINK.jpg" title="Anisalidrus Turban - Turban Femme Pretty Pink" id="img"></div></td>
-        <td><div class="one_image"><img src="images/FEMME-CHOCO-GREY.jpg" title="Anisalidrus Turban - Turban Femme Choco Grey" id="img"></div></td>
-        <td><div class="one_image"><img src="images/FEMME-MINT-GREEN.jpg" title="Anisalidrus Turban - Turban Femme Mint Green" id="img"></div></td>
-      </tr>
-      <tr>
-        <td><p id="name">Anisalidrus Turban - Turban Femme Baby Blue</p></td>
-        <td><p id="name">Anisalidrus Turban - Turban Femme Pretty Pink</p></td>
-        <td><p id="name">Anisalidrus Turban - Turban Femme Choco Grey</p></td>
-        <td><p id="name">Anisalidrus Turban - Turban Femme Mint Green</p></td>
-      </tr>
-      <tr>
-        <td><text id="price">RM75.00</text></td>
-        <td><text id="price">RM75.00</text></td>
-        <td><text id="price">RM75.00</text></td>
-        <td><text id="price">RM75.00</text></td>
-      </tr>
-      <tr>
-        <td><button type="submit" name="turban1" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="turban2" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="turban3" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="turban4" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-      </tr>
-    </table></br>
 
-    <table class="turban_table">
-      <tr>
-        <td><div class="one_image"><img src="images/DENIM-BLUE.jpg" title="Anisalidrus Turban - Denim Blue" id="img"></div></td>
-        <td><div class="one_image"><img src="images/DENIM-GREY.jpg" title="Anisalidrus Turban – Denim Grey" id="img"></div></td>
-        <td><div class="one_image"><img src="images/DENIM-MUSTARD.jpg" title="Anisalidrus Turban – Denim Mustard" id="img"></div></td>
-        <td><div class="one_image"><img src="images/DENIM-GREEN.jpg" title="Anisalidrus Turban – Denim Green" id="img"></div></td>
-      </tr>
-      <tr>
-        <td><p id="name">Anisalidrus Turban - Denim Blue</p></td>
-        <td><p id="name">Anisalidrus Turban – Denim Grey</p></td>
-        <td><p id="name">Anisalidrus Turban – Denim Mustard</p></td>
-        <td><p id="name">Anisalidrus Turban – Denim Green</p></td>
-      </tr>
-      <tr>
-        <td><text id="price">RM55.00</text></td>
-        <td><text id="price">RM55.00</text></td>
-        <td><text id="price">RM55.00</text></td>
-        <td><text id="price">RM55.00</text></td>
-      </tr>
-      <tr>
-        <td><button type="submit" name="turban5" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="turban6" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="turban7" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="turban8" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-      </tr>
-    </table></br>
+    <div class="all_image">
 
-    <table class="turban_table">
-      <tr>
-        <td><div class="one_image"><img src="images/DENIM-PINK.jpg" title="Anisalidrus Turban - Denim Pink" id="img"></div></td>
-        <td><div class="one_image"><img src="images/BASIC-BLUE.jpg" title="Anisalidrus Turban – Basic Blue" id="img"></div></td>
-        <td><div class="one_image"><img src="images/BASIC-WHITE.jpg" title="Anisalidrus Turban – Basic White" id="img"></div></td>
-      </tr>
-      <tr>
-        <td><p id="name">Anisalidrus Turban - Denim Pink</p></td>
-        <td><p id="name">Anisalidrus Turban – Basic Blue</p></td>
-        <td><p id="name">Anisalidrus Turban – Basic White</p></td>
-      </tr>
-      <tr>
-        <td><text id="price">RM55.00</text></td>
-        <td><text id="price">RM50.00</text></td>
-        <td><text id="price">RM50.00</text></td>
-      </tr>
-      <tr>
-        <td><button type="submit" name="turban9" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="turban10" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="turban11" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-      </tr>
-    </table></br>
+      <?php
+          $query = "SELECT * FROM products WHERE category = 'Turban' ORDER BY id ASC";
+          $result = mysqli_query($con,$query);
+          if(mysqli_num_rows($result) > 0) {
 
-  
+              while ($row = mysqli_fetch_array($result)) {
 
-   
-  </div>
-</article>
+                  ?>
+                  <form method="post" action="turban.php?action=add&id=<?php echo $row["id"]; ?>">
+                    <div>
+                    <table class="turban_table">
+                        <tr>
+                          <td id="row-img"><div class="one_image"><img src="<?php echo $row["image"]; ?>" title="<?php echo $row["name"]; ?>" id="img"></div></td>
+                        </tr>
+                        <tr>
+                          <td id="row-name"><p id="name"><?php echo $row["name"]; ?></p></td>
+                        </tr>
+                        <tr>
+                          <td id="row-price"><text id="price"><?php echo 'RM'.$row["price"]; ?></text></td>
+                        </tr>
+                        <input type="hidden" name="quantity" value="1";>
+                        <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>">
+                        <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>">
+                        <input type="hidden" name="hidden_img" value="<?php echo $row["image"]; ?>">
+                        <tr>
+                          <td><button type="submit" name="add" class="addCartBtn">ADD TO CART</button></td>
+                        </tr>
+                        </table>
+                      </div>
+                  </form>
+                  <?php
+              }
+          }
+      ?>
+      </br>
+    </div>
+
+  </article>
   <?php include 'scarf-footer.inc.php';?>
 </body>
 </html>
-
