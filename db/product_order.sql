789e799e4a93045e293ffd0a23ab06cfbb4f9169
alyaamn <71474184+alyaamn@users.noreply.github.com>
2020-10-03 19:34:26 +0800

    Update product_order.sql

--- db/product_order.sql
+++ db/product_order.sql
@@ -4,7 +4,7 @@
 CREATE TABLE `product_order` (
 	`po_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`product_id` int(8) NOT NULL,
-	`user_id` int(11) NOT NULL,
+	`order_id` int(11) NOT NULL,
 	`quantity` int(11) NOT NULL,
 	`subtotal` decimal(6,2) NOT NULL,
 	FOREIGN KEY (product_id) REFERENCES PRODUCTS(product_id),
