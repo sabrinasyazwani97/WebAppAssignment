9893579cbe48d30b3d4849e3fbbf7ce25ed9d586
Nur Shakina <70265335+shakinassi@users.noreply.github.com>
2020-10-03 16:35:40 +0800

    Update bawal.php

--- bawal.php
+++ bawal.php
@@ -1,9 +1,11 @@
+<?php include 'add-bawal-tocart.php';?>
+
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <title>Bawal Collection | Scarf4u</title>
-  <link rel="stylesheet" href="style.css" />
-  <link href="bawal.css" rel="stylesheet"/>
+  <link rel="stylesheet" href="css/style.css" />
+  <link href="css/bawal.css" rel="stylesheet"/>
   <script src="script.js"></script>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,900" rel="stylesheet">
@@ -11,147 +13,49 @@
 
 <body>
   <?php include 'scarf-header.inc.php';?>
-<article class="bawal">
-
-  <div class="all_image">
+  <article class="bawal">
     <h1 id="bawal-title">BAWAL COLLECTION</h1>
-    <table class="bawal_table">
-      <tr>
-        <td><div class="one_image"><img src="images/Fairy Green.jpg" title="Kekaboo – Fairy Green" id="img"></div></td>
-        <td><div class="one_image"><img src="images/Blush Pink.jpg" title="Kekaboo – Blush Pink" id="img"></div></td>
-        <td><div class="one_image"><img src="images/Coffee Au Lait.jpg" title="Kekaboo – Coffee Au Lait" id="img"></div></td>
-        <td><div class="one_image"><img src="images/Paddlepop.jpg" title="Kekaboo – Paddlepop" id="img"></div></td>
-      </tr>
-      <tr>
-        <td><p id="name">Kekaboo – Fairy Green</p></td>
-        <td><p id="name">Kekaboo – Blush Pink</p></td>
-        <td><p id="name">Kekaboo – Coffee Au Lait</p></td>
-        <td><p id="name">Kekaboo – Paddlepop</p></td>
-      </tr>
-      <tr>
-        <td><text id="price">RM25.00</text></td>
-        <td><text id="price">RM25.00</text></td>
-        <td><text id="price">RM25.00</text></td>
-        <td><text id="price">RM25.00</text></td>
-      </tr>
-      <tr>
-        <td><button type="submit" name="bawal1" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="bawal2" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="bawal3" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="bawal4" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-      </tr>
-    </table></br>
 
-    <table class="bawal_table">
-      <tr>
-        <td><div class="one_image"><img src="images/Puteri Tiana.jpg" title="Kekaboo – Puteri Tiana" id="img"></div></td>
-        <td><div class="one_image"><img src="images/Safiya Medina.jpg" title="Kekaboo – Safiya Medina" id="img"></div></td>
-        <td><div class="one_image"><img src="images/Malikha Aura.jpg" title="Kekaboo – Malikha Aura" id="img"></div></td>
-        <td><div class="one_image"><img src="images/Ameyrah Bella.jpg" title="Kekaboo – Ameyrah Bella" id="img"></div></td>
-      </tr>
-      <tr>
-        <td><p id="name">Kekaboo – Puteri Tiana</p></td>
-        <td><p id="name">Kekaboo – Safiya Medina</p></td>
-        <td><p id="name">Kekaboo – Malikha Aura</p></td>
-        <td><p id="name">Kekaboo – Ameyrah Bella</p></td>
-      </tr>
-      <tr>
-        <td><text id="price">RM25.00</text></td>
-        <td><text id="price">RM25.00</text></td>
-        <td><text id="price">RM25.00</text></td>
-        <td><text id="price">RM25.00</text></td>
-      </tr>
-      <tr>
-        <td><button type="submit" name="bawal5" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="bawal6" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="bawal7" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="bawal8" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-      </tr>
-    </table></br>
+    <div class="all_image">
 
-    <table class="bawal_table">
-      <tr>
-        <td><div class="one_image"><img src="images/Zayton Aisya.jpg" title="Kekaboo – Zayton Aisya" id="img"></div></td>
-        <td><div class="one_image"><img src="images/Liyana Zahra.jpg" title="Kekaboo – Liyana Zahra" id="img"></div></td>
-        <td><div class="one_image"><img src="images/Mirabelle Sri.jpg" title="Kekaboo – Mirabelle Sri Hanim" id="img"></div></td>
-        <td><div class="one_image"><img src="images/MARIA CARLA.jpg" title="Mirabelle Maria Clara" id="img"></div></td>
-      </tr>
-      <tr>
-        <td><p id="name">Kekaboo – Zayton Aisya</p></td>
-        <td><p id="name">Kekaboo – Liyana Zahra</p></td>
-        <td><p id="name">Kekaboo – Mirabelle Sri Hanim</p></td>
-        <td><p id="name">Mirabelle Maria Clara</p></td>
-      </tr>
-      <tr>
-        <td><text id="price">RM25.00</text></td>
-        <td><text id="price">RM25.00</text></td>
-        <td><text id="price">RM20.00</text></td>
-        <td><text id="price">RM20.00</text></td>
-      </tr>
-      <tr>
-        <td><button type="submit" name="bawal9" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="bawal10" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="bawal11" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="bawal12" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-      </tr>
-    </table></br>
+      <?php
+          $query = "SELECT * FROM products WHERE category = 'Bawal' ORDER BY id ASC";
+          $result = mysqli_query($con,$query);
+          if(mysqli_num_rows($result) > 0) {
 
-    <table class="bawal_table">
-      <tr>
-        <td><div class="one_image"><img src="images/VIOLET AURORA.jpg" title="Mirabelle Violet Aurora" id="img"></br></div></br></td>
-        <td><div class="one_image"><img src="images/ISOBELLE.jpg" title="Mirabelle Isobelle" id="img"></div></td>
-        <td><div class="one_image"><img src="images/Daisie.jpg" title="Kekaboo – Mirabelle Daisie" id="img"></div></td>
-        <td><div class="one_image"><img src="images/Kadija.jpg" title="Kekaboo – Mirabelle Kadija" id="img"></div></td>
-      </tr>
-      <tr>
-        <td><p id="name">Mirabelle Violet Aurora</p></td>
-        <td><p id="name">Mirabelle Isobelle</p></td>
-        <td><p id="name">Kekaboo – Mirabelle Daisie</p></td>
-        <td><p id="name">Kekaboo – Mirabelle Kadija</p></td>
-      </tr>
-      <tr>
-        <td><text id="price">RM20.00</text></td>
-        <td><text id="price">RM20.00</text></td>
-        <td><text id="price">RM20.00</text></td>
-        <td><text id="price">RM20.00</text></td>
-      </tr>
-      <tr>
-        <td><button type="submit" name="bawal3" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="bawal14" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="bawal15" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="bawal16" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-      </tr>
-    </table></br>
+              while ($row = mysqli_fetch_array($result)) {
 
-    <table class="bawal_table">
-      <tr>
-        <td><div class="one_image"><img src="images/Louisa.jpg" title="Kekaboo – Mirabelle Louisa" id="img"></div></td>
-        <td><div class="one_image"><img src="images/Rossaluna.jpg" title="Kekaboo – Mirabelle Rossaluna" id="img"></div></td>
-        <td><div class="one_image"><img src="images/Amalea.jpg" title="Kekaboo – Mirabelle Amalea" id="img"></div></td>
-        <td><div class="one_image"><img src="images/Violetta.jpg" title="Kekaboo – Mirabelle Violetta" id="img"></div></td>      
-	  </tr>
-      <tr>
-        <td><p id="name">Kekaboo – Mirabelle Louisa</p></td>
-        <td><p id="name">Kekaboo – Mirabelle Rossaluna</p></td>
-        <td><p id="name">Kekaboo – Mirabelle Amalea</p></td>
-        <td><p id="name">Kekaboo – Mirabelle Violetta</p></td>
-      </tr>
-      <tr>
-        <td><text id="price">RM20.00</text></td>
-        <td><text id="price">RM20.00</text></td>
-        <td><text id="price"><text id="oldprice"/>RM20.00 </text><text id="promo"> RM15.00</text></text></td>
-        <td><text id="price">RM20.00</text></td>
-      </tr>
-      <tr>
-        <td><button type="submit" name="bawal17" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="bawal18" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="bawal19" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-        <td><button type="submit" name="bawal20" class="addCartBtn" onclick="document.location='cart.php'">ADD TO CART</button></td>
-      </tr>
-    </table></br>
-  </div>
-</article>
+                  ?>
+                  <form method="post" action="bawal.php?action=add&id=<?php echo $row["id"]; ?>">
+                    <div>
+                    <table class="bawal_table">
+                        <tr>
+                          <td id="row-img"><div class="one_image"><img src="<?php echo $row["image"]; ?>" title="<?php echo $row["name"]; ?>" id="img"></div></td>
+                        </tr>
+                        <tr>
+                          <td id="row-name"><p id="name"><?php echo $row["name"]; ?></p></td>
+                        </tr>
+                        <tr>
+                          <td id="row-price"><text id="price"><?php echo 'RM'.$row["price"]; ?></text></td>
+                        </tr>
+                        <input type="hidden" name="quantity" value="1";>
+                        <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>">
+                        <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>">
+                        <input type="hidden" name="hidden_img" value="<?php echo $row["image"]; ?>">
+                        <tr>
+                          <td><button type="submit" name="add" class="addCartBtn">ADD TO CART</button></td>
+                        </tr>
+                        </table>
+                      </div>
+                  </form>
+                  <?php
+              }
+          }
+      ?>
+      </br>
+    </div>
+
+  </article>
   <?php include 'scarf-footer.inc.php';?>
 </body>
 </html>
-
