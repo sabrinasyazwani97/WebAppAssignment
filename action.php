8b65e7b9a51a5aad568a09455ff3ac663a7e84d8
Sabrina Mazlan <54005211+sabrinasyazwani97@users.noreply.github.com>
2020-10-06 13:03:45 +0800

    Update action.php

--- action.php
+++ action.php
@@ -1,1 +1,32 @@
-//session
+<?php
+
+//Register - (insert into user table) *shakina
+
+//Login - START session uid (select from user table) *alyaa
+
+//Header - *edit header.php *shakina
+
+//My account  *nad
+
+//details (select from user table)
+
+//Recent order (select from order table)
+
+//Update address (update user table)
+
+//Add to cart - (insert into cart) *sab
+
+//Update - (update cart :qty) *sab
+
+//Delete product - (delete from cart) *sab
+
+//Checkout - (select from user : details) *sab
+
+//Before place order - (insert into orders, product_order) *shakina
+
+//After place order - (delete uid from cart) *alyaa
+
+//Logout - Destroy session uid *alyaa
+
+
+?>
