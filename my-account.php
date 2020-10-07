4cbb0e12e9aa9b61e76215fe7cc3511963b211d6
Nurul Nadiah binti Zulkifli <53247760+nadiahzulkifli98@users.noreply.github.com>
2020-10-07 21:42:09 +0800

    Update my-account.php

--- my-account.php
+++ my-account.php
@@ -18,13 +18,11 @@
 <body>
 
 	<!-- HEADER -->
-    <?php include 'scarf-header.inc.php'; 
+    <?php 
     
-        // start session
-        session_start();
+        include 'scarf-header.inc.php'; 
 
-        $id = $_SESSION['user_id'];
-        $username = $_SESSION['username'];
+        $fname = include 'action.php'; 
     ?>
 
 	
@@ -46,40 +44,11 @@
         <div id="content">
             <h2>My Account Details</h2>
             <div style="border-top: 1px dotted #ffdcba;"></div><br/>
-            <p><h3>Welcome <?php echo $username; ?>!</h3></p><br/> 
+            <p><h3>Welcome <?php if(isset($fname)) echo $fname?>!</h3></p><br/> 
         
             <?php
-                $conn = mysqli_connect("localhost", "root", "", "scarf4u");
-                $query = "SELECT * FROM users WHERE user_id = '$id'";
-
-                if ($result = $conn->query($query)) {
-                    while ($row = $result->fetch_assoc()) {
-                        $field1name = $row["email"];
-                        $field2name = $row["fname"];
-                        $field3name = $row["lname"];
-                        $field4name = $row["contact_num"];
-                        $field5name = $row["street_1"];
-                        $field6name = $row["street_2"];
-                        $field7name = $row["zipcode"];
-                        $field8name = $row["city"];
-                        $field9name = $row["states"];
-                        $field10name = $row["country"];
-
-                        echo "Email: <b>" .$field1name."</b><br/><br/>";
-                        echo "First name: <b>" .$field2name."</b><br/><br/>";
-                        echo "Last name: <b>" .$field3name."</b><br/><br/>";
-                        echo "Contact number: <b>" .$field4name."</b><br/><br/><br/>";
-                        echo "<div style='border-top: 2px dashed #bb4c71;'></div>";
-                        echo "<h2>My Address</h2>";
-                        echo "<div style='border-top: 1px dotted #ffdcba;'></div><br/>";
-                        echo "Street 1: <b>" .$field5name."</b><br/><br/>";
-                        echo "Street 2: <b>" .$field6name."</b><br/><br/>";
-                        echo "Zipcode: <b>" .$field7name."</b><br/><br/>";
-                        echo "City: <b>" .$field8name.", ".$field9name."</b><br/><br/>";
-                        echo "State: <b>" .$field10name."</b><br/><br/>";
-                    }
-                    $result->free();
-                } 
+		include 'action.php';
+                myAccount();
             ?>
         </div>
     </div>
@@ -89,4 +58,5 @@
 	<?php include 'scarf-footer.inc.php'; ?>
 		
 </body>
+</html>
 </html>
