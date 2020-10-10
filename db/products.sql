9ac763688ab4469de1b7c32938235dd662ff93b6
Sabrina Mazlan <54005211+sabrinasyazwani97@users.noreply.github.com>
2020-10-10 16:21:07 +0800

    Update products.sql

--- db/products.sql
+++ db/products.sql
@@ -1,28 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 05, 2020 at 08:30 AM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.4.7
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `scarf4u`
---
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `products`
 --
@@ -135,8 +110,4 @@ INSERT INTO `products` (`product_id`, `name`, `code`, `image`, `price`, `categor
 ALTER TABLE `products`
   ADD PRIMARY KEY (`product_id`),
   ADD UNIQUE KEY `product_code` (`code`);
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
